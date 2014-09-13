7d47105185901d75b3dca746fafabfb3eef98ef4
schildren <schildren@gmail.com>
2014-09-13 19:32:28 -0400

    Setup
    Inicial

---
+++ MyApiMovie.Translator/TokenAccess/AdmAuthentication.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyApiMovie.Translator.TokenAccess
+{
+    public class AdmAuthentication
+    {
+        public static readonly string DatamarketAccessUri = Configuration.DatamarketAccessUri;
+        private string clientId;
+        private string cientSecret;
+        private string request;
+
+        public AdmAuthentication(string clientId, string clientSecret)
+        {
+            this.clientId = clientId;
+            this.cientSecret = clientSecret;
+            //If clientid or client secret has special characters, encode before sending request
+            this.request = string.Format(Configuration.UriClientCredentials, HttpUtility.UrlEncode(clientId), HttpUtility.UrlEncode(clientSecret));
+        }
+
+        public AdmAccessToken GetAccessToken()
+        {
+            return HttpPost(DatamarketAccessUri, this.request);
+        }
+
+        private AdmAccessToken HttpPost(string DatamarketAccessUri, string requestDetails)
+        {
+            //Prepare OAuth request 
+            WebRequest webRequest = WebRequest.Create(DatamarketAccessUri);
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Method = "POST";
+            byte[] bytes = Encoding.ASCII.GetBytes(requestDetails);
+            webRequest.ContentLength = bytes.Length;
+            using (Stream outputStream = webRequest.GetRequestStream())
+            {
+                outputStream.Write(bytes, 0, bytes.Length);
+            }
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AdmAccessToken));
+                //Get deserialized object from JSON stream
+                AdmAccessToken token = (AdmAccessToken)serializer.ReadObject(webResponse.GetResponseStream());
+                return token;
+            }
+        }
+    }
+}
---
+++ MyApiMovie.Translator/Translator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Web;
+using System.Media;
+using MyApiMovie.Translator.TokenAccess;
+
+namespace MyApiMovie.Translator
+{
+    public class Translator
+    {
+        public string sourceText { set; get; }
+        public string fromLanguage { set; get; }
+        public string toLanguage { set; get; }
+        string URI = Configuration.TranslatorURI;
+
+        public Translator() { }
+
+        
+        public string Translate()
+        {
+            TokenAccess.AdmAccessToken admToken;
+            string headerValue;
+            TokenAccess.AdmAuthentication admAuth = new TokenAccess.AdmAuthentication(Configuration.TranslatorUserId, Configuration.TranslatorUserKey);
+            try
+            {
+                admToken = admAuth.GetAccessToken();
+                headerValue = "Bearer " + admToken.access_token;
+                return Translate(headerValue);
+            }
+            catch (WebException e)
+            {
+                ProcessWebException(e);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+            }
+            return "";
+        }
+        private static void ProcessWebException(WebException e)
+        {
+            Console.WriteLine("{0}", e.ToString());
+            string strResponse = string.Empty;
+            using (HttpWebResponse response = (HttpWebResponse)e.Response)
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(responseStream, System.Text.Encoding.ASCII))
+                    {
+                        strResponse = sr.ReadToEnd();
+                    }
+                }
+            }
+            Console.WriteLine("Http status code={0}, error message={1}", e.Status, strResponse);
+        }
+
+        private string Translate(string authToken)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(string.Format(URI, System.Web.HttpUtility.UrlEncode(sourceText), fromLanguage, toLanguage));
+            httpWebRequest.Headers.Add("Authorization", authToken);
+            WebResponse response = null;
+            try
+            {
+                response = httpWebRequest.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(Type.GetType("System.String"));
+                    return (string)dcs.ReadObject(stream);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                    response = null;
+                }
+            }
+        }
+
+    }
+}
